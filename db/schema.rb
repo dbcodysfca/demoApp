5adac2d70ac4a0b8c699d3aebabb679d9feeff32
Doug Cody <dougcody@mac.com>
2011-09-17 10:28:10 -0700

    Added Microposts resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110912032008) do
+ActiveRecord::Schema.define(:version => 20110917172300) do
+
+  create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
